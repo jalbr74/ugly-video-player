19ef5809c2bf99a4ee1ee876e3385973fcad28ee
jalbr98 <jalbr98@gmail.com>
2025-03-25 14:18:03 -0600

    Added a fullscreen button... kind of
    added the ability to maybe go fullscreen

--- src/player/JaredsPlayer.tsx
+++ src/player/JaredsPlayer.tsx
@@ -1,38 +1,58 @@
 // React Template
-import { MediaController } from 'media-chrome/react';
-import { useRef } from 'react';
+import { MediaController, MediaMuteButton, MediaPlayButton } from 'media-chrome/react';
+import 'media-chrome/react/media-theme';
+import { useRef, useState } from 'react';
 import MuxVideo from '@mux/mux-video-react';
 
-interface PlayerProps {
-    isPlaying: boolean;
-}
-
-export function JaredsPlayer ({ isPlaying }: PlayerProps) {
+export function DadsPlayer () {
+    const [isPlaying, setPlaying] = useState(false);
     const videoRef = useRef<HTMLVideoElement>(null);
+
+
+    const elementRef = useRef(null);
+
+    const resizeElement = () => {
+        if (elementRef.current) {
+            elementRef.current.style.width = Math.floor(Math.random() * 151) + '%'; // Set your desired width
+        }
+    };
 
     const togglePlay = () => {
         if (videoRef.current) {
             if (isPlaying) {
                 videoRef.current.pause();
+                setPlaying(false);
             } else {
                 videoRef.current.play();
+                setPlaying(true);
             }
         }
     }
 
     return (
         <>
-            <button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play'}</button>
-
-            <MediaController id="player">
+            <MediaController ref={elementRef} id="player">
                 <MuxVideo
                     ref={videoRef}
-                    playbackId="PLtkNjmv028bYRJr8BkDlGw7SHOGkCl4d"
+                    playbackId="rX2AIcBEIQ01QkyD5CLPEwyfaGr39tRUGzHqRBggVAvU"
                     slot="media"
                     crossOrigin="anonymous"
+                    // controls
                     muted
                 />
+                {/*<MediaPlayButton />*/}
+                {/*<MediaMuteButton />*/}
             </MediaController>
+
+            <div className="drawer-container">
+                <div className="trigger"></div>
+                <div className="drawer">
+                    <ul>
+                        <li><button onClick={togglePlay}>{isPlaying ? 'Pause' : 'Play' }</button></li>
+                    </ul>
+                </div>
+            </div>
+            <button onClick={resizeElement} id="resize-button">Fullscreen (maybe)</button>
         </>
     );
 }
